3498deb10ad1aab9b30a7407efd0fb0c22ac43a9
vial72075 <68506560+vial72075@users.noreply.github.com>
2020-07-19 19:56:06 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_a2():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a2)
---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function on_forever() {
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a2() {
+    
+})
